10805574e19d74f6c3e8d9085386c8b1c3ce1133
priyangshu24 <138028617+priyangshu24@users.noreply.github.com>
2025-06-16 22:37:30 +0530

    12: agents data table

--- src/modules/agents/ui/views/agents-view.tsx
+++ src/modules/agents/ui/views/agents-view.tsx
@@ -5,14 +5,24 @@ import {  useSuspenseQuery } from "@tanstack/react-query";
 import { useTRPC } from "@/trpc/client";
 import { LoadingState } from "@/components/loading-state";
 import { ErrorState } from "@/components/error-state";
+import { DataTable } from "../components/data-table";
+import { columns} from "../components/columns";
+import { EmptyState } from "@/components/empty-state";
 
 export const AgentsView = () => {
     const trpc = useTRPC();
     const { data } = useSuspenseQuery(trpc.agent.getMany.queryOptions());
     
     return (
-        <div>
-            {JSON.stringify(data, null, 2)}
+        <div className="flex-1 pb-4 px-4 md:px-8 flex-col gap-y-4">
+            <DataTable data={data} columns={columns}/> 
+            {data.length === 0 && (
+                <EmptyState
+                    title="Create your first agent"
+                    description="Agents are the building blocks of your AI. Create your first agent to get started."
+                
+                />
+            )}
         </div>
     );
 };
@@ -31,6 +41,6 @@ export const AgentsViewError = () => {
     <ErrorState 
             title="Loading Agent Error" 
             description="Please try again later"
-            />
+    />
     )   
 }
