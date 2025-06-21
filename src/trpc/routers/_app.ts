4f4872c1a93d93db47946bf28e350c53c459d8a1
priyangshu24 <138028617+priyangshu24@users.noreply.github.com>
2025-06-21 23:01:10 +0530

    feat(meetings): add meetings module with views and trpc procedures
    - Create meetings page with dynamic routing for individual meetings
    - Implement meetings view components with loading and error states
    - Add meetings table schema to database
    - Set up trpc router with getOne and getMany procedures
    - Configure prefetching and hydration for meetings data

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,10 +1,13 @@
 import { createTRPCRouter } from "../init";
 
 import { agentRouter } from "@/modules/agents/server/procedures";
+import { meetingsRouter } from "@/modules/meetings/server/procedures";
+
 
 export const appRouter =  createTRPCRouter({
 
     agent: agentRouter,
+    meetings: meetingsRouter,
 })
 // export type defination of API
 export type AppRouter = typeof appRouter;
